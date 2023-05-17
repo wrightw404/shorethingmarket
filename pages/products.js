6c1eef8d7ea844ccbb6f2c6a9ea9979fbb66d25b
Will <wrightw404@gmail.com>
2023-05-17 15:20:55 -0700

    refactoring on products page

--- pages/products.js
+++ pages/products.js
@@ -61,6 +61,7 @@ export default function Products() {
         <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-sedona-orange pl-10">We Have Everything You Need</h1>
             <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl pl-10">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
+            
         </div>       
     </section>
 
