0ad5cb6cb85c5f8efd579ee0246625fc30960653
Will <wrightw404@gmail.com>
2023-06-05 18:58:35 -0400

    small changes

--- pages/fuel.js
+++ pages/fuel.js
@@ -13,10 +13,19 @@ export default function Fuel() {
     <div className="max-w-screen-lg mx-auto py-20 pl-4">
       <Image className="mx-auto rounded-xl" src={water}></Image>
     </div>
-      <div className="py-40 text-center">
+      <div className="py-20 text-center">
         <h1 className="text-sedona-orange mb-4 text-4xl font-extrabold tracking-tight leading-none sm:text-3xl md:text-5xl xl:text-6xl">Wacky Wednesday is officially back for the 2023 season!</h1>
-        <h2 className="pt-11 text-lg sm:text-sm lg:text-lg xl:text-xl font-semibold">All grades of fuel will be shore price</h2>
+        <h2 className="pt-1 text-lg sm:text-sm lg:text-lg xl:text-xl font-semibold">All grades of fuel will be shore price</h2>
         <h4 className="text-sm lg:text-lg xl:text-xl font-semibold py-3">Non ethanol rec fuel shore price every day of the week</h4>
+        <h2 className="pt-7 text-2xl sm:text-lg lg:text-2xl xl:text-3xl font-bold">BP fuels with invigorate work to clean your engine and prevent buildup</h2>
+        <h2 className="text-xl sm:text-lg lg:text-2xl xl:text-3xl font-bold">BP fuels with invigorate work to clean your engine and prevent buildup</h2>
+        <ul className="text-left">
+          <li className="text-sm lg:text-lg xl:text-xl py-3">Reg 87</li>
+          <li className="text-sm lg:text-lg xl:text-xl py-3">Mid Grade 89</li>
+          <li className="text-sm lg:text-lg xl:text-xl py-3">Premium 93</li>
+          <li className="text-sm lg:text-lg xl:text-xl py-3">Diesel</li>
+          <li className="text-sm lg:text-lg xl:text-xl py-3">Rec Fuel - Non Ethanol 90</li>
+        </ul>
         <button className="rounded-full text-sm lg:text-sm xl:text-lg px-2 py-1 bg-sedona-orange hover:bg-mountain-blue hover:scale-75"><a href="https://www.bp.com/en_us/united-states/home/products-and-services/bpme-rewards/bpme-rewards-visa.html" target={"_blank"}>Learn about BPMe Rewards</a></button>
       </div>
     
--- pages/products.js
+++ pages/products.js
@@ -32,6 +32,16 @@ export default function Products() {
   return (
     <section className= "bg-gradient-to-b from-mountain-blue to-white">
    <Nav />
+   <section className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex bg-cover">
+            <Image src={alc} className="rounded-xl bg-contain" alt="mockup"></Image>
+        </div>
+        <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-sedona-orange pl-10">We Have Liqour!</h1>
+            <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-white pl-10">Extensive collection of spirits</p>
+           
+        </div>       
+    </section>
    <div className="py-11 text-center">
    <h1 className="text-sedona-orange mb-4 text-4xl font-extrabold tracking-tight leading-none sm:text-3xl md:text-5xl xl:text-6xl ">We Gurantee We Have What You're Looking For</h1>
    <h3 className="px-10 sm:text-sm xl:text-xl"> From groceries to motor oil, we have it all!</h3>
